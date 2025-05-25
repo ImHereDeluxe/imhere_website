bfa8987d89017476da8d5c0935ff17929baaa5bb
saranin-programmer <aldan2550@gmail.com>
2025-05-25 11:01:26 +0700

    v.0.0.008.1
    1) ладно, не буду материться

--- src/shared/features/version/Version.tsx
+++ src/shared/features/version/Version.tsx
@@ -5,7 +5,7 @@ import './Version.css';
 const Version = () => {
     return (
         <div className="version-label">
-            pre-tiny-alpha-mama-papa-huy-version v0.0.001 - чел..
+            pre-tiny-alpha-mama-papa-huy-version v0.0.001 - ну поч
         </div>
     );
 };
